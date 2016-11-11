d4d4a44947dff87c472cc1ce08f8c3b52ff26545
Saumitra Bose <imtoobose@gmail.com>
2016-11-11 21:07:39 +0530

    Added login page that opens only if not logged in, as well as firebase authentication

--- app/src/main/java/io/github/deepbluecitizenservice/citizenservice/LoginActivity.java
+++ app/src/main/java/io/github/deepbluecitizenservice/citizenservice/LoginActivity.java
@@ -1,9 +1,12 @@
 package io.github.deepbluecitizenservice.citizenservice;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.auth.api.Auth;
@@ -12,10 +15,20 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private GoogleApiClient mGAP;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String TAG = "Login Activity";
     private static int RC_SIGN_IN = 1;
 
     @Override
@@ -24,6 +37,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         setContentView(R.layout.activity_login);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
                 .requestEmail()
                 .build();
 
@@ -32,18 +46,50 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "Firebase logged in: " + user.getUid());
+                } else {
+                    Log.d(TAG, "Firebase logged out");
+                }
+            }
+        };
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGAP.connect();
+        mAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthListener);
+        mGAP.disconnect();
+    }
+
+    //Attach intent for signing in to a button
     public void handleButtonClick(View view){
         Intent SignInIntent = Auth.GoogleSignInApi.getSignInIntent(mGAP);
         startActivityForResult(SignInIntent, RC_SIGN_IN);
     }
+
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 
+    //After signing in, handle the return
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -54,14 +100,53 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         }
     }
 
+
+    //Get the sign in account if successful login and account exists
+    //Then navigate to the main activity
     protected void handleSignInResult(GoogleSignInResult result){
         if(result.isSuccess()){
             GoogleSignInAccount account = result.getSignInAccount();
-            Intent startMainActivity = new Intent(this, MainActivity.class);
-            if(account!=null) {
-                startMainActivity.putExtra("name", account.getDisplayName());
-                startActivity(startMainActivity);
-            }
+            firebaseAuthWithGoogle(account);
         }
+    }
+
+    //Authenticate firebase with google account
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account){
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+
+        final GoogleSignInAccount act = account;
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                        }
+
+                        Intent startMainActivity = new Intent(getBaseContext(), MainActivity.class);
+                        startMainActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                        SharedPreferences prefs = getSharedPreferences(getString(R.string.user_preferences_id), Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = prefs.edit();
+
+                        editor.putBoolean(getString(R.string.logged_in_state), true);
+                        editor.putString(getString(R.string.user_name), act.getDisplayName());
+                        editor.putString(getString(R.string.user_email), act.getEmail());
+
+                        editor.apply();
+
+                        startActivity(startMainActivity);
+                    }
+                });
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        moveTaskToBack(true);
     }
 }
--- app/src/main/java/io/github/deepbluecitizenservice/citizenservice/MainActivity.java
+++ app/src/main/java/io/github/deepbluecitizenservice/citizenservice/MainActivity.java
@@ -1,13 +1,76 @@
 package io.github.deepbluecitizenservice.citizenservice;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private final String TAG = "Main Activity:";
+    private boolean isLoggedIn = false;
+    private GoogleApiClient mGAP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestEmail()
+                .build();
+
+        mGAP = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.user_preferences_id), Context.MODE_PRIVATE);
+        isLoggedIn = prefs.getBoolean(getString(R.string.logged_in_state), false);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user!=null){
+            Log.d(TAG, "Current user is: "+ user.getDisplayName());
+        }
+
+        if(!isLoggedIn || user==null){
+            Intent startLoginActivity = new Intent(this, LoginActivity.class);
+            startActivity(startLoginActivity);
+        }
+    }
+
+    public void handleButtonClick(View v){
+        if(mGAP.isConnected()) {
+            Auth.GoogleSignInApi.signOut(mGAP).setResultCallback(
+                    new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(Status status) {
+                            Log.d(TAG, "Logged out");
+                            Intent login = new Intent(getBaseContext(), LoginActivity.class);
+                            startActivity(login);
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
