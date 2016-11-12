5009a337ee797f48bbcf947788fdf452001cf6c1
Saumitra Bose <imtoobose@gmail.com>
2016-11-12 12:10:56 +0530

    Updated Gradle wrapper version to 2.2.2

--- app/src/main/java/io/github/deepbluecitizenservice/citizenservice/MainActivity.java
+++ app/src/main/java/io/github/deepbluecitizenservice/citizenservice/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             Auth.GoogleSignInApi.signOut(mGAP).setResultCallback(
                     new ResultCallback<Status>() {
                         @Override
-                        public void onResult(Status status) {
+                        public void onResult(@NonNull Status status) {
                             Log.d(TAG, "Logged out");
                             SharedPreferences prefs = getSharedPreferences(getString(R.string.user_preferences_id), MODE_PRIVATE);
                             SharedPreferences.Editor editor = prefs.edit();
