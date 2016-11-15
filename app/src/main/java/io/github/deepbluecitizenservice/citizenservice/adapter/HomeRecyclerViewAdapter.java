28b8bba3d8551b628d9881661c4e8176d282a91d
CCD <dcunha.cyprien@gmail.com>
2016-11-15 20:46:43 +0530

    Add items one-by-one to recycleview instead of firebase adapter

--- app/src/main/java/io/github/deepbluecitizenservice/citizenservice/adapter/HomeRecyclerViewAdapter.java
+++ app/src/main/java/io/github/deepbluecitizenservice/citizenservice/adapter/HomeRecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package io.github.deepbluecitizenservice.citizenservice.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,87 +12,100 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.util.HashMap;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
+import io.github.deepbluecitizenservice.citizenservice.ExpImageActivity;
 import io.github.deepbluecitizenservice.citizenservice.MapsActivity;
 import io.github.deepbluecitizenservice.citizenservice.R;
 import io.github.deepbluecitizenservice.citizenservice.database.ProblemModel;
 
-public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerViewAdapter.ViewHolder>{
+public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerViewAdapter.ViewHolder> {
     private Context context;
     private List<ProblemModel> problemList;
 
-
-    public HomeRecyclerViewAdapter(Context context, List <ProblemModel> problemList){
+    public HomeRecyclerViewAdapter(Context context, List<ProblemModel> problems){
         this.context = context;
-        this.problemList = problemList;
+        this.problemList = problems;
+    }
+
+    public void addProblem(ProblemModel problemModel){
+        problemList.add(problemModel);
+        notifyDataSetChanged();
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View card = LayoutInflater.from(context).inflate(R.layout.problem_card, null);
-        return new HomeRecyclerViewAdapter.ViewHolder(card);
+        return new ViewHolder(card);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         ProblemModel problem = problemList.get(position);
-        HashMap <String, Object> Getter = problem.getDetails();
 
-        holder.userNameTV.setText((String)Getter.get(ProblemModel.USER_NAME));
-        holder.locationTV.setText((String)Getter.get(ProblemModel.LOCATIONADDRESS));
-        holder.categoryTV.setText((String)Getter.get(ProblemModel.CATEGORY));
-        holder.periodTV.setText((String) Getter.get(ProblemModel.TIMECREATED));
-        holder.descriptionTV.setText((String)Getter.get(ProblemModel.DESCRIPTION));
+        holder.userNameTV.setText(problem.creatorName);
+        holder.locationTV.setText(problem.locationAddress);
+        holder.categoryTV.setText(problem.getCategory());
+        holder.periodTV.setText(problem.getPeriod());
+        holder.descriptionTV.setText(problem.description);
 
-        StorageReference ref = FirebaseStorage.getInstance().getReferenceFromUrl((String)Getter.get(ProblemModel.URL));
-        Glide.with(context)
-                .using(new FirebaseImageLoader())
-                .load(ref)
-                .crossFade()
-                .into(holder.imageView);
+        String noOfImagesString = getNoOfImagesText(problem);
+        if(noOfImagesString != null)
+            holder.noOfImagesTV.setText(noOfImagesString);
+        else
+            holder.noOfImagesTV.setVisibility(View.GONE);
 
-        Glide.with(context)
-                .load((String)Getter.get(ProblemModel.USER_URL))
-                .crossFade()
-                .into(holder.userImage);
+        StorageReference refProblem = FirebaseStorage.getInstance().getReference(problem.url);
+        Glide.with(context).using(new FirebaseImageLoader()).load(refProblem).into(holder.imageView);
+        Glide.with(context).load(problem.creatorURL).into(holder.userImage);
 
         setExpandButtonListener(holder.expandButton, holder.descriptionTV);
-
-        setLocationClickListener(holder.locationTV,
-                (String)Getter.get(ProblemModel.USER_NAME), (String)Getter.get(ProblemModel.CATEGORY),
-                (Double)Getter.get(ProblemModel.LOCATIONX), (Double)Getter.get(ProblemModel.LOCATIONY),
-                (String)Getter.get(ProblemModel.TIMECREATED));
-
-        //setImageClickListener(holder.imageView, problem);
+        setLocationClickListener(holder.locationTV, problem);
+        setImageClickListener(holder.imageView, problem);
     }
 
-    @Override
-    public int getItemCount() {
-        return problemList.size();
+    private String getNoOfImagesText(ProblemModel p){
+        //TODO: Needed if we have multiple images
+        int no = 0;
+        if(no == 0) return null;
+        else if(no == 1) return "+1 Image";
+        else return "+" + no + " Images";
     }
 
-//    public void setImageClickListener(ImageView image){
-//        Bitmap bitmap =Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.ARGB_8888);
-//
-//    }
+    private void setImageClickListener(final ImageView img, final ProblemModel p){
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bitmap bitmap = ((GlideBitmapDrawable) img.getDrawable()).getBitmap();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                byte[] imageBytes = stream.toByteArray();
 
-    private void setLocationClickListener(TextView locationTV, final String creator, final String category,
-                                          final double locationX, final double locationY, final String timeCreated) {
+                Intent intent = new Intent(context, ExpImageActivity.class);
+                intent.putStringArrayListExtra(ExpImageActivity.URL_LIST, new ArrayList<String>());
+                intent.putExtra(ExpImageActivity.IMAGE_PARCEL, imageBytes);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    private void setLocationClickListener(TextView locationTV, final ProblemModel problem) {
         locationTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String title = creator + " (" + category + ")";
+                String title = problem.creatorName + " (" + problem.getCategory() + ")";
 
                 Intent intent = new Intent(context, MapsActivity.class);
-                intent.putExtra(MapsActivity.MAP_LOC_X, locationX);
-                intent.putExtra(MapsActivity.MAP_LOC_Y, locationY);
-                intent.putExtra(MapsActivity.MAP_SNIPPET, timeCreated);
+                intent.putExtra(MapsActivity.MAP_LOC_X, problem.locationX);
+                intent.putExtra(MapsActivity.MAP_LOC_Y, problem.locationY);
+                intent.putExtra(MapsActivity.MAP_SNIPPET, problem.getPeriod());
                 intent.putExtra(MapsActivity.MAP_TITLE, title);
 
                 context.startActivity(intent);
@@ -117,8 +131,12 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerVi
         });
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
-        String id;
+    @Override
+    public int getItemCount() {
+        return problemList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView userNameTV;
         TextView locationTV;
@@ -131,22 +149,21 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerVi
         ImageView imageView;
         ImageView expandButton;
 
-        public ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
+
             userNameTV = (TextView) itemView.findViewById(R.id.user_name_tv);
             locationTV = (TextView) itemView.findViewById(R.id.location_tv);
             categoryTV = (TextView) itemView.findViewById(R.id.category_tv);
             periodTV = (TextView) itemView.findViewById(R.id.time_period_tv);
             descriptionTV = (TextView) itemView.findViewById(R.id.description_tv);
             noOfImagesTV = (TextView) itemView.findViewById(R.id.no_of_images_tv);
+
+
             userImage = (ImageView) itemView.findViewById(R.id.user_image);
             imageView = (ImageView) itemView.findViewById(R.id.image_view);
+
             expandButton = (ImageView) itemView.findViewById(R.id.arrow_button);
         }
-
-        public void setId(String id){
-            this.id = id;
-        }
     }
-
 }
