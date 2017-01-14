2002b464c52af391e48643e059f98c1ea3a99265
CCD-1997 <dcunha.cyprien@gmail.com>
2017-01-14 12:15:35 +0530

    Add no internet connection card

--- app/src/main/java/io/github/deepbluecitizenservice/citizenservice/MainActivity.java
+++ app/src/main/java/io/github/deepbluecitizenservice/citizenservice/MainActivity.java
@@ -4,17 +4,25 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
@@ -271,6 +279,36 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             settingsBaseView.setPadding(0, 0, 0, height);
             photoFragmentBaseView.setPadding(0, 0, 0, height);
         }
+    }
+
+    public void setupNoInternetCard(CardView noInternetCard){
+        TypedValue typedValue = new TypedValue();
+        Resources.Theme theme = getTheme();
+        theme.resolveAttribute(R.attr.colorControlActivated, typedValue, true);
+        noInternetCard.setBackgroundColor(typedValue.data);
+
+        TextView openSettings = (TextView) noInternetCard.findViewById(R.id.no_internet_connection);
+        openSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Settings.ACTION_SETTINGS));
+            }
+        });
+    }
+
+    public boolean checkInternetConnectivity(CardView card){
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean isNetworkAvailable = activeNetworkInfo != null && activeNetworkInfo.isConnected();
+
+        if(card != null)
+            if(!isNetworkAvailable)
+                card.setVisibility(View.VISIBLE);
+            else
+                card.setVisibility(View.GONE);
+
+        return isNetworkAvailable;
     }
 
     @Override
